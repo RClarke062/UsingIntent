c5e8e360167d77c86d4e0807404b7442bc6f1e99
RClarke062 <robertjclarke@sympatico.ca>
2018-11-11 11:48:47 -0500

    Initial commit

---
+++ app/src/main/java/com/robboapps/usingintent/SecondActivity.java
@@ -0,0 +1,28 @@
+/**
+ * Created by rclarke on 10/8/2017.
+ */
+package com.robboapps.usingintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SecondActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void onClick(View view) {
+        Intent data = new Intent();
+        // ---get the EditText view---
+        EditText txt_username = (EditText) findViewById(R.id.txtUsername);
+        // ---set the data to pass back---
+        data.setData(Uri.parse( txt_username.getText().toString()));
+        setResult(RESULT_OK, data);
+        //---closes the activity---
+        finish();
+    }
+}
